9d5a8d2ddf3d820b73c77d4fc0e4a18ccdc7d03a
Bryan Lundberg <bryanlundberg@outlook.com>
2024-12-25 12:02:41 -0600

    style: update website design

--- main.js
+++ main.js
@@ -6,37 +6,38 @@ render();
 const player = document.querySelector("twisty-player");
 const moves = [];
 
-/**
- * Key listener for cube movements.
- */
-document.addEventListener("keyup", (e) => {
-  const movesMap = {
-    f: { move: () => window.CubeEngine.rotateU(false), notation: "U'" },
-    j: { move: () => window.CubeEngine.rotateU(true), notation: "U" },
-    g: { move: () => window.CubeEngine.rotateF(false), notation: "F'" },
-    h: { move: () => window.CubeEngine.rotateF(true), notation: "F" },
-    i: { move: () => window.CubeEngine.rotateR(true), notation: "R" },
-    k: { move: () => window.CubeEngine.rotateR(false), notation: "R'" },
-    d: { move: () => window.CubeEngine.rotateL(true), notation: "L" },
-    e: { move: () => window.CubeEngine.rotateL(false), notation: "L'" },
-    t: { move: () => window.CubeEngine.rotateX(true), notation: "x" },
-    y: { move: () => window.CubeEngine.rotateX(true), notation: "x" },
-    b: { move: () => window.CubeEngine.rotateX(false), notation: "x'" },
-    n: { move: () => window.CubeEngine.rotateX(false), notation: "x'" },
-    ñ: { move: () => window.CubeEngine.rotateY(true), notation: "y" },
-    a: { move: () => window.CubeEngine.rotateY(false), notation: "y'" },
-    s: { move: () => window.CubeEngine.rotateD(true), notation: "D" },
-    l: { move: () => window.CubeEngine.rotateD(false), notation: "D'" },
-  };
+const movesMap = {
+  f: { move: () => window.CubeEngine.rotateU(false), notation: "U'" },
+  j: { move: () => window.CubeEngine.rotateU(true), notation: "U" },
+  g: { move: () => window.CubeEngine.rotateF(false), notation: "F'" },
+  h: { move: () => window.CubeEngine.rotateF(true), notation: "F" },
+  i: { move: () => window.CubeEngine.rotateR(true), notation: "R" },
+  k: { move: () => window.CubeEngine.rotateR(false), notation: "R'" },
+  d: { move: () => window.CubeEngine.rotateL(true), notation: "L" },
+  e: { move: () => window.CubeEngine.rotateL(false), notation: "L'" },
+  t: { move: () => window.CubeEngine.rotateX(true), notation: "x" },
+  y: { move: () => window.CubeEngine.rotateX(true), notation: "x" },
+  b: { move: () => window.CubeEngine.rotateX(false), notation: "x'" },
+  n: { move: () => window.CubeEngine.rotateX(false), notation: "x'" },
+  ñ: { move: () => window.CubeEngine.rotateY(true), notation: "y" },
+  a: { move: () => window.CubeEngine.rotateY(false), notation: "y'" },
+  s: { move: () => window.CubeEngine.rotateD(true), notation: "D" },
+  l: { move: () => window.CubeEngine.rotateD(false), notation: "D'" },
+};
 
-  const key = e.key.toLowerCase();
+/**
+ * Execute a move and update the UI.
+ * @param {string} key - The key corresponding to the movement.
+ */
+function executeMove(key) {
   const moveObj = movesMap[key];
 
   if (moveObj) {
+    // Execute the movement
     moveObj.move();
     moves.push(moveObj.notation);
 
-    // sync UI
+    // Update the user interface
     document.querySelector("#total").textContent = moves.length;
     document.querySelector("#moves").textContent = moves.join(" ");
     document.querySelector(
@@ -45,6 +46,21 @@ document.addEventListener("keyup", (e) => {
     player.alg = moves.join(" ");
     render();
   }
+}
+
+// setInterval for random movements
+setInterval(() => {
+  const keys = Object.keys(movesMap);
+  const randomKey = keys[Math.floor(Math.random() * keys.length)];
+  executeMove(randomKey);
+}, 1000);
+
+/**
+ * Key listener for cube movements.
+ */
+document.addEventListener("keyup", (e) => {
+  const key = e.key.toLowerCase();
+  executeMove(key);
 });
 
 function render() {
